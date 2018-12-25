25ea2f1a2d889c01c0ff196d2daf2a5837b2c0c8
Administrator <295435732@qq.com>
2018-12-25 18:19:05 +0800

    20181225-01

--- src/main/java/com/algorithm/demo/Sort.java
+++ src/main/java/com/algorithm/demo/Sort.java
@@ -111,11 +111,95 @@ public class Sort {
         LOG.info("selectSort:{}", log);
     }
 
+    /**
+     * 插入排序
+     *
+     * 从第一个元素开始，该元素可以认为已经被排序
+     * 取出下一个元素，在已经排序的元素序列中从后向前扫描
+     * 如果该元素（已排序）大于新元素，将该元素移到下一位置
+     * 重复步骤3，直到找到已排序的元素小于或者等于新元素的位置
+     * 将新元素插入到该位置中
+     * 重复步骤2
+     * @param numbers  待排序数组
+     */
+    public static void insertSort(int[] numbers)
+    {
+        int size = numbers.length;
+        int temp = 0 ;
+        int j =  0;
+
+        for(int i = 0 ; i < size ; i++)
+        {
+            //取标志位
+            temp = numbers[i];
+
+            //假如temp比前面的值小，则将前面的值后移
+            for(j = i ; j > 0 && temp < numbers[j-1] ; j --)
+            {
+                numbers[j] = numbers[j-1];
+            }
+
+            //j--
+            numbers[j] = temp;
+        }
+    }
+
     public static void main(String[] args) throws JsonProcessingException {
        int sort[] = {49,27,13,76,97,65,38,49};
         /* selectSort(sort);*/
-        bubbleSort(sort);
+        insertSort(sort);
+        LOG.info("---:{}",MAPPER.writeValueAsString(sort));
 
+        int[] a={6,7,4,5,8};
+        int arrayLength=a.length;
+        //循环建堆
+        for(int i=0;i<arrayLength-1;i++){
+            //建堆
+            buildMaxHeap(a,arrayLength-1-i);
+            //交换堆顶和最后一个元素
+            swap(a,0,arrayLength-1-i);
+            LOG.info("---:{}-->>{}",i,MAPPER.writeValueAsString(a));
+        }
+
+        LOG.info("---:{}",MAPPER.writeValueAsString(a));
 
     }
+
+    //对data数组从0到lastIndex建大顶堆
+    public static void buildMaxHeap(int[] data, int lastIndex){
+        //从lastIndex处节点（最后一个节点）的父节点开始
+        for(int i=(lastIndex-1)/2;i>=0;i--){
+            //k保存正在判断的节点
+            int k=i;
+            //如果当前k节点的子节点存在
+            while(k*2+1<=lastIndex){
+                //k节点的左子节点的索引
+                int biggerIndex=2*k+1;
+                //如果biggerIndex小于lastIndex，即biggerIndex+1代表的k节点的右子节点存在
+                if(biggerIndex<lastIndex){
+                    //若果右子节点的值较大
+                    if(data[biggerIndex]<data[biggerIndex+1]){
+                        //biggerIndex总是记录较大子节点的索引
+                        biggerIndex++;
+                    }
+                }
+                //如果k节点的值小于其较大的子节点的值
+                if(data[k]<data[biggerIndex]){
+                    //交换他们
+                    swap(data,k,biggerIndex);
+                    //将biggerIndex赋予k，开始while循环的下一次循环，重新保证k节点的值大于其左右子节点的值
+                    k=biggerIndex;
+                }else{
+                    break;
+                }
+            }
+        }
+    }
+    //交换
+    private static void swap(int[] data, int i, int j) {
+        int tmp=data[i];
+        data[i]=data[j];
+        data[j]=tmp;
+    }
+
 }
